1bbb54da6f9ccd20c60a98102e5595a18aa791c8
PARAISO NELSON <NELSONITO@air-de-paraiso.home>
2024-04-08 06:07:42 +0200

    last

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react(), eslint()],
   build: {
     rollupOptions: {
-      external: ['js-cookie', '@fortawesome/react-fontawesome']
+      external: ['js-cookie', '@fortawesome/react-fontawesome', '@fortawesome/free-solid-svg-icons']
     }
   }
 });
