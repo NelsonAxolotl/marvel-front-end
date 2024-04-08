1c10f4558358cdeb515ceff93ae36a6479ee260f
PARAISO NELSON <NELSONITO@air-de-paraiso.home>
2024-04-08 15:31:31 +0200

    modified

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,7 @@ import eslint from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
-  build: {
-    rollupOptions: {
-      external: ['js-cookie', '@fortawesome/react-fontawesome', '@fortawesome/free-solid-svg-icons']
-    }
-  }
-});
+
+}
+);
 
